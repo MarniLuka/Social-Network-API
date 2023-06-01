340aeee1b8995e158675b7e7417543283f72c262
Marni Luka <marnixluka@gmail.com>
2023-06-01 18:08:19 -0400

    updated userRoute

--- routes/api/userRoute.js
+++ routes/api/userRoute.js
@@ -1,5 +1,28 @@
 const router = require('express').Router();
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    newFriend,
+    deleteFriend
+} = require ('../../controllers/userController');
 
+// localhost:3001/api/users
+router.route('/')
+    .get(getUser)
+    .post(createUser);
 
+// localhost:3001/api/users/:userId
+router.route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser)
+
+// localhost:3001/api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId')
+    .post(newFriend)
+    .delete(deleteFriend)
 
 module.exports = router;
