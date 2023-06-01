43c2a9b05446a46ef902ccbdc3b412070220c0b4
Marni Luka <marnixluka@gmail.com>
2023-06-01 18:15:17 -0400

    updated thoughtRoute

--- routes/api/thoughtRoute.js
+++ routes/api/thoughtRoute.js
@@ -1,5 +1,28 @@
 const router = require('express').Router();
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    newReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
 
+// localhost:3001/api/thoughts
+router.route('/')
+    .get(getThoughts)
+    .post(createThought)
 
+// localhost:3001/api/thoughts/:thoughtId
+router.route('/:thoughtId')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(deleteThought)
+
+// localhost:3001/api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+    .post(newReaction)
+    .delete(deleteReaction)
 
 module.exports = router;
