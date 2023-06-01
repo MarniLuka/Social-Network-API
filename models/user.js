4d203011a8c0be58bbc13dcf7a9c7d32c480cfcd
Marni Luka <marnixluka@gmail.com>
2023-05-31 20:02:22 -0400

    created user model

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+        // regex for email validation. (double check if it works...)
+        match: /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Thought"
+        },
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        },
+    ],
+},
+{
+    toJSON: {
+        virtuals: true
+    },
+    id: false
+});
+
+userSchema.virutal('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User
